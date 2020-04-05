d05e27e2d373398c952c0e083562f3740cb46347
PetterBraka <Petter.braka@gmail.com>
2020-04-05 19:26:48 +0200

    created classes for day and drink with initialisers.

---
+++ Pods/pods.h
@@ -0,0 +1,19 @@
+//
+//  pods.h
+//  pods
+//
+//  Created by Petter vang Brakalsvålet on 04/04/2020.
+//  Copyright © 2020 Petter vang Brakalsvålet. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for pods.
+FOUNDATION_EXPORT double podsVersionNumber;
+
+//! Project version string for pods.
+FOUNDATION_EXPORT const unsigned char podsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <pods/PublicHeader.h>
+
+
