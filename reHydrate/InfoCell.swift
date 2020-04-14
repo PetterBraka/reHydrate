db658d4684560cdf1b17e1b1ac1a2ae8977a028a
PetterBraka <petter.braka@gmail.com>
2020-04-14 20:37:00 +0200

    the history page will now shoe information based on day, but will always show for the current day.

--- reHydrate/InfoCell.swift
+++ reHydrate/InfoCell.swift
@@ -9,8 +9,20 @@
 import UIKit
 
 class InfoCell: UITableViewCell {
+    @IBOutlet weak var titleOfCard: UILabel!
     @IBOutlet weak var typeOfDrink: UILabel!
     @IBOutlet weak var amountOfDrink: UILabel!
     
-
+    func setLabels(_ drink: Drink, _ possistion: Int){
+        switch possistion {
+        case 0:
+            titleOfCard.text = "Goal:"
+        case 1:
+            titleOfCard.text = "Consumed:"
+        default:
+            break
+        }
+        typeOfDrink.text = drink.typeOfDrink
+        amountOfDrink.text = String(drink.amountOfDrink)
+    }
 }
