4b68ee306333fef22edc47dfcf0966c434a2dd03
PetterBraka <Petter.braka@gmail.com>
2020-04-14 20:37:00 +0200

    the history page will now shoe information based on day, but will always show for the current day.

--- reHydrate/CalendarVC.swift
+++ reHydrate/CalendarVC.swift
@@ -9,25 +9,45 @@
 import UIKit
 
 class CalendarVC: UIViewController {
+    
+    var drinks: [Drink] = []
+    var days: [Day] = []
+    
+    @IBOutlet weak var tableView: UITableView!
     @IBAction func exit(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        days = Day.loadDay()
+        getDay(Date.init())
+        tableView.isScrollEnabled = false
+        tableView.delegate = self
+        tableView.dataSource = self
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func getDay(_ dateOfDay: Date){
+        let formatter = DateFormatter()
+        formatter.dateFormat = "EEEE - dd/MM/yy"
+        let day: Day = days.first(where: {formatter.string(from: $0.date) == formatter.string(from: dateOfDay)})!
+        drinks.append(day.goalAmount)
+        drinks.append(day.consumedAmount)
     }
-    */
+}
 
+extension CalendarVC: UITableViewDelegate, UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "customCell") as! InfoCell
+        cell.setLabels(drinks[indexPath.row], indexPath.row)
+        cell.selectionStyle = .none
+        return cell
+    }
+    
+    
 }
