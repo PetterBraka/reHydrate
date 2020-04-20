0b6932c18cddc341893d6483e352c37e840c947d
PetterBraka <Petter.braka@gmail.com>
2020-04-20 12:22:43 +0200

    added an option to remove all saved data

--- reHydrate/ViewControllers/AboutVC.swift
+++ reHydrate/ViewControllers/AboutVC.swift
@@ -10,19 +10,40 @@ import UIKit
 
 class AboutVC: UIViewController {
     
-    @IBOutlet weak var imageToturial: UIImageView!
     @IBOutlet weak var helpButton: UIButton!
+    @IBOutlet weak var exitButton: UIButton!
+    @IBOutlet weak var clearButton: UIButton!
     
     @IBAction func exit(_ sender: UIButton) {
-        imageToturial.isHidden = true
         self.dismiss(animated: true, completion: nil)
+    }
+    @IBAction func clearData(_ sender: Any) {
+        let clearDataAlert = UIAlertController(title: "Clearing data", message: "are you sure you want to delete all save data?", preferredStyle: .alert)
+        clearDataAlert.addAction(UIAlertAction(title: "keep data", style: .cancel, handler: nil))
+        clearDataAlert.addAction(UIAlertAction(title: "remove data", style: .destructive, handler: {_ in
+            let domain = Bundle.main.bundleIdentifier!
+            UserDefaults.standard.removePersistentDomain(forName: domain)
+            UserDefaults.standard.synchronize()}))
+        
+        self.present(clearDataAlert, animated: true, completion: nil)
     }
     
     @objc func tap(_ sender: UIGestureRecognizer){
         switch sender.view {
             case helpButton:
                 print("help pressed")
-                imageToturial.isHidden = false
+                let helpImage = UIImageView.init(image: UIImage.init(named: "toturial-1"))
+                self.view.addSubview(helpImage)
+                
+                helpImage.translatesAutoresizingMaskIntoConstraints = false
+                helpImage.leadingAnchor.constraint(equalTo: self.view.leadingAnchor).isActive = true
+                helpImage.trailingAnchor.constraint(equalTo: self.view.trailingAnchor).isActive = true
+                helpImage.topAnchor.constraint(equalTo: self.exitButton.bottomAnchor).isActive = true
+                helpImage.bottomAnchor.constraint(equalTo: self.view.bottomAnchor).isActive = true
+                helpImage.contentMode = .scaleAspectFit
+                helpButton.isHidden = true
+                clearButton.isHidden = true
+                
             default:
             break
         }
@@ -30,7 +51,6 @@ class AboutVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        imageToturial.isHidden = true
         setUpButtons()
         // Do any additional setup after loading the view.
     }
@@ -41,8 +61,12 @@ class AboutVC: UIViewController {
         helpButton.layer.borderColor = UIColor.lightGray.cgColor
         helpButton.contentEdgeInsets = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
         
-        let helpTapGesture = UITapGestureRecognizer(target: self, action: #selector(tap))
+        clearButton.layer.borderWidth = 3
+        clearButton.layer.cornerRadius = 20
+        clearButton.layer.borderColor = UIColor.red.cgColor
+        clearButton.contentEdgeInsets = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
         
+        let helpTapGesture = UITapGestureRecognizer(target: self, action: #selector(tap))
         helpButton.addGestureRecognizer(helpTapGesture)
         
     }
