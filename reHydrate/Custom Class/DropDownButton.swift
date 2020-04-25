59ed05303aa9b3df909a2435364e3f3807862d2d
PetterBraka <petter.braka@gmail.com>
2020-04-25 15:45:22 +0200

    Set up the settings page with foldable sections

--- reHydrate/Custom Class/DropDownButton.swift
+++ reHydrate/Custom Class/DropDownButton.swift
@@ -14,15 +14,15 @@ protocol dropDownMenuProtocol {
 }
 
 class DropDownButton: UIButton, dropDownMenuProtocol {
-    var dropView = DropDownMenu()
-    var height = NSLayoutConstraint()
-    var isOpen = false
+    var dropView 	= DropDownMenu()
+    var height 		= NSLayoutConstraint()
+    var isOpen 		= false
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        dropView = DropDownMenu.init(frame: CGRect.init(x: 0, y: 0, width: 0, height: 0))
-        dropView.delegate = self
-        dropView.translatesAutoresizingMaskIntoConstraints = false
+        dropView 											= DropDownMenu.init(frame: CGRect.init(x: 0, y: 0, width: 0, height: 0))
+        dropView.delegate 									= self
+        dropView.translatesAutoresizingMaskIntoConstraints 	= false
     }
     
     required init?(coder: NSCoder) {
@@ -37,23 +37,21 @@ class DropDownButton: UIButton, dropDownMenuProtocol {
     override func didMoveToSuperview() {
         self.superview?.addSubview(dropView)
         self.superview?.bringSubviewToFront(dropView)
-        dropView.topAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
-        dropView.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
-        dropView.widthAnchor.constraint(equalTo: self.widthAnchor).isActive = true
+        dropView.topAnchor.constraint(equalTo: self.bottomAnchor).isActive 			= true
+        dropView.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive 	= true
+        dropView.widthAnchor.constraint(equalTo: self.widthAnchor).isActive 		= true
         height = dropView.heightAnchor.constraint(equalToConstant: 0)
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if isOpen == false {
-            isOpen = true
-            
+        if isOpen 	== false {
+            isOpen 	= true
             NSLayoutConstraint.deactivate([self.height])
-            self.height.constant = 150
-//            if self.dropView.tableView.contentSize.height > 150 {
-//                self.height.constant = 150
-//            } else {
-//                self.height.constant = self.dropView.tableView.contentSize.height
-//            }
+            if self.dropView.tableView.contentSize.height > 150 {
+                self.height.constant = 150
+            } else {
+                self.height.constant = self.dropView.tableView.contentSize.height
+            }
             
             NSLayoutConstraint.activate([self.height])
             UIView.animate(withDuration: 0.8, delay: 0, usingSpringWithDamping: 0.2, initialSpringVelocity: 0.5, options: .curveEaseInOut, animations: {
@@ -79,24 +77,24 @@ class DropDownButton: UIButton, dropDownMenuProtocol {
 }
 
 class DropDownMenu: UIView, UITableViewDelegate, UITableViewDataSource{
-    var dropDownMenuOptions = [String]()
-    var tableView = UITableView()
+    var dropDownMenuOptions 	= [String]()
+    var tableView 				= UITableView()
     var delegate: dropDownMenuProtocol!
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         
-        tableView.backgroundColor = UIColor.red
-        self.backgroundColor = UIColor.red
-        tableView.delegate = self
-        tableView.dataSource = self
+        tableView.backgroundColor 	= UIColor.red
+        self.backgroundColor 		= UIColor.red
+        tableView.delegate 			= self
+        tableView.dataSource 		= self
         self.addSubview(tableView)
         
-        tableView.translatesAutoresizingMaskIntoConstraints = false
-        tableView.leftAnchor.constraint(equalTo: self.leftAnchor).isActive = true
-        tableView.rightAnchor.constraint(equalTo: self.rightAnchor).isActive = true
-        tableView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
-        tableView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+        tableView.translatesAutoresizingMaskIntoConstraints 					= false
+        tableView.leftAnchor.constraint(equalTo: self.leftAnchor).isActive 		= true
+        tableView.rightAnchor.constraint(equalTo: self.rightAnchor).isActive 	= true
+        tableView.topAnchor.constraint(equalTo: self.topAnchor).isActive 		= true
+        tableView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive 	= true
         
     }
     
@@ -113,10 +111,9 @@ class DropDownMenu: UIView, UITableViewDelegate, UITableViewDataSource{
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell()
-        
-        cell.textLabel?.text = dropDownMenuOptions[indexPath.row]
-        cell.backgroundColor = UIColor.darkGray
+        let cell 				= UITableViewCell()
+        cell.textLabel?.text 	= dropDownMenuOptions[indexPath.row]
+        cell.backgroundColor 	= UIColor.darkGray
         return cell
     }
     
