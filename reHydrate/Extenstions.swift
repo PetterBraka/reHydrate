10efbd967f61020be0228cf61f57ac5be77c52cc
PetterBraka <Petter.braka@gmail.com>
2020-09-03 16:26:29 +0100

    the app will now only show one decimal if needed.

--- reHydrate/Extenstions.swift
+++ reHydrate/Extenstions.swift
@@ -145,6 +145,6 @@ extension Double {
      ```
      */
     var clean: String {
-        return self.truncatingRemainder(dividingBy: 1) == 0 ? String(format: "%.0f", self) : String(format: "%.2f", self)
+        return self.truncatingRemainder(dividingBy: 1) == 0 ? String(format: "%.0f", self) : String(format: "%.1f", self)
     }
 }
