479b044f4165fbdd11ef765eac1b81f29b5baf7a
PetterBraka <petter.braka@gmail.com>
2020-10-20 12:21:15 +0100

    Added improved fetching functions to the watch app so it can work on its own. Have also added a print function for Day for debugging.

--- reHydrate-watch Extension/InterfaceController.swift
+++ reHydrate-watch Extension/InterfaceController.swift
@@ -53,9 +53,7 @@ class InterfaceController: WKInterfaceController {
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
         // Configure interface objects here.
-        fetchDays()
-        //ask the phone for the goal and the consumed amount.
-        
+        days = fetchDays()
         if WCSession.isSupported(){
             let session = WCSession.default
             session.delegate = self
@@ -102,26 +100,42 @@ class InterfaceController: WKInterfaceController {
     }
     
     //MARK: - Load and Save days
-    
-    func fetchDays() {
+
+    /// Loading all days saved to core data.
+    /// - Returns: *[Day]* an array off days loaded
+    public func fetchDays() -> [Day] {
         do {
-            self.days = try self.context.fetch(Day.fetchRequest())
+            let days = try context.fetch(Day.fetchRequest()) as! [Day]
+            #if DEBUG
+            days.forEach({$0.toPrint()})
+            #endif
+            return days
         } catch {
             print("can't featch days")
             print(error.localizedDescription)
+            return [Day()]
         }
     }
-    
-    func saveDays() {
+
+    /// Remove a specific day
+    /// - Parameter day: The day you want to remove
+    public func removeDay(day : Day){
+        context.delete(day)
+    }
+
+    /// Tries to save all days created in context
+    public func saveDays() {
         do {
-            try self.context.save()
+            try context.save()
         } catch {
             print("can't save days")
             print(error.localizedDescription)
         }
     }
-    
-    func fetchToday() -> Day {
+
+    /// Will get a day representing data for the current day. It will also set the goal to be the same as the goal from the previous day.
+    /// - Returns: The day found to be equal to todays date.
+    public func fetchToday() -> Day {
         do {
             let request = Day.fetchRequest() as NSFetchRequest
             // Get today's beginning & tomorrows beginning time
@@ -134,23 +148,65 @@ class InterfaceController: WKInterfaceController {
             request.predicate = datePredicate
             // tries to get the day out of the array.
             let loadedDays = try self.context.fetch(request)
+            #if DEBUG
+            loadedDays.forEach({$0.toPrint()})
+            #endif
             // If today wasn't found it will create a new day.
             guard let today = loadedDays.first else {
-                let today = Day(context: self.context)
+                // create new day
+                let today = Day(context: context)
                 today.date = Date()
-                today.goal = 3
-                return today }
+                // tries to get yesterday data
+                let yesterdayDate = Calendar.current.date(byAdding: .day, value: -1, to: today.date)
+                let yesterday = fetchDay(yesterdayDate!)
+                today.goal = yesterday?.goal ?? 3
+                
+                return today
+            }
             return today
         } catch {
             print("can't featch day")
             print(error.localizedDescription)
             // If the loading of data fails, we create a new day
-            let today = Day(context: self.context)
+            let today = Day(context: context)
             today.date = Date()
-            today.goal = 3
+            // tries to get yesterday data
+            let yesterdayDate = Calendar.current.date(byAdding: .day, value: -1, to: today.date)
+            let yesterday = fetchDay(yesterdayDate!)
+            today.goal = yesterday?.goal ?? 3
             return today
         }
     }
+
+    /// Will featch and return a spesific day or reurn nill if day can't be found for the date.
+    /// - Parameter date: The date for the spesific day you wan't
+    /// - Returns: returns *Day* if found else nil
+    public func fetchDay(_ date: Date) -> Day? {
+        do {
+            let request = Day.fetchRequest() as NSFetchRequest
+            // Get day's beginning & tomorrows beginning time
+            let dateFrom = Calendar.current.startOfDay(for: date)
+            let dateTo = Calendar.current.date(byAdding: .day, value: 1, to: dateFrom)
+            // Sets conditions for date to be within day
+            let fromPredicate = NSPredicate(format: "date >= %@", dateFrom as NSDate)
+            let toPredicate = NSPredicate(format: "date < %@", dateTo! as NSDate)
+            let datePredicate = NSCompoundPredicate(andPredicateWithSubpredicates: [fromPredicate, toPredicate])
+            request.predicate = datePredicate
+            // tries to get the day out of the array.
+            let loadedDays = try self.context.fetch(request)
+            #if DEBUG
+            loadedDays.forEach({$0.toPrint()})
+            #endif
+            // If the day wasn't found it will create a new day.
+            let day = loadedDays.first
+            return day
+        } catch {
+            print("can't featch day")
+            print(error.localizedDescription)
+            return nil
+        }
+    }
+
     
     func updateSummary(){
         let today = fetchToday()
