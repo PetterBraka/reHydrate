ec11365c7c7ae689bc13dc62a7fd6992759377b6
PetterBraka <Petter.braka@gmail.com>
2021-11-23 11:56:13 +0000

    Improved  transitions

--- reHydrate/ViewModel/AppViewModel.swift
+++ reHydrate/ViewModel/AppViewModel.swift
@@ -20,9 +20,11 @@ final class AppViewModel: ObservableObject {
     private var subscriptions = Set<AnyCancellable>()
     
     @Published var currenState: AppState = .home
+    @Published var previousState: AppState = .home
     
     private func navigate(to state: AppState) {
         withAnimation {
+            previousState = currenState
             currenState = state
         }
     }
