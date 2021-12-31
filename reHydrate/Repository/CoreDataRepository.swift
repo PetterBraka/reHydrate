038703c54817a10e87f710090e3ded60881156c3
PetterBraka <Petter.braka@gmail.com>
2021-12-31 19:29:40 +0000

    Recreated coreData functions with async await

--- reHydrate/Repository/CoreDataRepository.swift
+++ reHydrate/Repository/CoreDataRepository.swift
@@ -13,6 +13,18 @@ import UIKit
 protocol Repository {
     associatedtype Day
 
+    func create() async throws -> Day
+    func delete(_ day: Day)
+    func get(id: String,
+             predicate: NSPredicate?,
+             sortDescriptors: [NSSortDescriptor]?) async throws -> Day?
+    func get(date: Date,
+             predicate: NSPredicate?,
+             sortDescriptors: [NSSortDescriptor]?) async throws -> Day?
+    func getLatesGoal(predicate: NSPredicate?) async throws -> Day?
+    func getAll(predicate: NSPredicate?,
+                sortDescriptors: [NSSortDescriptor]?) async throws -> [Day]
+
     func create() -> AnyPublisher<Day, Error>
     func delete(_ day: Day) -> AnyPublisher<Bool, Error>
     func get(id: String, predicate: NSPredicate?, sortDescriptors: [NSSortDescriptor]?)
@@ -36,6 +48,82 @@ class CoreDataRepository<T: NSManagedObject>: Repository {
 
     init(context: NSManagedObjectContext) {
         managedObjectContext = context
+    }
+
+    func create() async throws -> T {
+        let className = String(describing: T.self)
+        guard let managedObject = NSEntityDescription.insertNewObject(forEntityName: className,
+                                                                      into: managedObjectContext) as? T
+        else {
+            throw CoreDataError.invalidManagedObjectType
+        }
+        return managedObject
+    }
+    
+    func delete(_ day: T) {
+        managedObjectContext.delete(day)
+    }
+    
+    func get(id: String,
+             predicate: NSPredicate?,
+             sortDescriptors: [NSSortDescriptor]?) async throws -> T? {
+        let entityName = String(describing: T.self)
+        let request = NSFetchRequest<T>(entityName: entityName)
+        request.sortDescriptors = sortDescriptors
+        request.predicate = predicate
+        request.predicate = NSPredicate(format: "id == %@", id)
+        if let elements = try? self.managedObjectContext.fetch(request) {
+            return elements.first
+        } else {
+            throw CoreDataError.invalidManagedObjectType
+        }
+    }
+    
+    func get(date: Date,
+             predicate: NSPredicate?,
+             sortDescriptors: [NSSortDescriptor]?) async throws -> T? {
+        let entityName = String(describing: T.self)
+        let request = NSFetchRequest<T>(entityName: entityName)
+        request.sortDescriptors = sortDescriptors
+        request.predicate = predicate
+        // Get day's beginning & tomorrows beginning time
+        let startOfDay = Calendar.current.startOfDay(for: date)
+        let startOfNextDay = Calendar.current.date(byAdding: .day, value: 1, to: startOfDay)
+        // Sets conditions for date to be within day
+        let fromPredicate = NSPredicate(format: "date >= %@", startOfDay as NSDate)
+        let toPredicate = NSPredicate(format: "date < %@", startOfNextDay! as NSDate)
+        let datePredicate = NSCompoundPredicate(andPredicateWithSubpredicates:
+            [fromPredicate, toPredicate])
+        request.predicate = datePredicate
+        guard let results = try? self.managedObjectContext.fetch(request) else {
+            throw CoreDataError.invalidManagedObjectType
+        }
+        return results.first
+    }
+    
+    func getLatesGoal(predicate: NSPredicate?) async throws -> T? {
+        let entityName = String(describing: T.self)
+        let request = NSFetchRequest<Day>(entityName: entityName)
+        request.sortDescriptors = [NSSortDescriptor(key: "date", ascending: false)]
+        request.predicate = predicate
+        request.fetchLimit = 1
+
+        guard let results = try? self.managedObjectContext.fetch(request) else {
+            throw CoreDataError.invalidManagedObjectType
+        }
+        return results.first
+    }
+    
+    func getAll(predicate: NSPredicate?,
+                sortDescriptors: [NSSortDescriptor]?) async throws -> [T] {
+        let entityName = String(describing: T.self)
+        let request = NSFetchRequest<T>(entityName: entityName)
+        request.sortDescriptors = sortDescriptors
+        request.predicate = predicate
+        guard let result = try? self.managedObjectContext.fetch(request) else {
+            throw CoreDataError.invalidManagedObjectType
+        }
+        return result
     }
 
     func create() -> AnyPublisher<T, Error> {
