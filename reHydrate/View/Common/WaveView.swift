155940cd58a3a723d76ef14c3276b086572ab32f
PetterBraka <Petter.braka@gmail.com>
2022-02-19 23:48:29 +0000

    Update waterView. View is now adaptable and will fill a container.

--- reHydrate/View/Common/WaveView.swift
+++ reHydrate/View/Common/WaveView.swift
@@ -8,21 +8,53 @@
 
 import SwiftUI
 
-struct WaveView: View {
-    @State private var percent = 50.0
-    @State private var waveOffset = Angle(degrees: 0)
+struct WaveView<Content: View>: View {
+    @State var color: Color
+    @State var offsetPercent: CGFloat
+    @State var isReversed: Bool = false
+    @Binding var waterLevel: CGFloat
+    @State var maxFillLevel: CGFloat = 0.7
+
+    @ViewBuilder var container: Content
 
     var body: some View {
-        VStack {
-            WaveShape(offset: Angle(degrees: self.waveOffset.degrees),
-                      percent: Double(percent) / 100)
-                .fill(Color.blue.opacity(0.8))
-        }
-        .onAppear {
-            withAnimation(Animation.linear(duration: 2)
-                .repeatForever(autoreverses: false)) {
-                self.waveOffset = Angle(degrees: 360)
+        ZStack {
+            TimelineView(.animation) { timeLine in
+                Canvas { context, size in
+                    let now = timeLine.date.timeIntervalSinceReferenceDate
+                    let angle = now.remainder(dividingBy: 2)
+                    let offset = angle * size.width
+                    context.translateBy(x: isReversed ? -offset : offset, y: 0)
+                    context.fill(getPath(size: size), with: .color(color))
+                    context.translateBy(x: -size.width, y: 0)
+                    context.fill(getPath(size: size), with: .color(color))
+                    context.translateBy(x: size.width * 2, y: 0)
+                    context.fill(getPath(size: size), with: .color(color))
+                }
             }
+            .opacity(0.8)
+            .mask {
+                container
+            }
+            container
+                .ignoresSafeArea()
+        }
+    }
+
+    func getPath(size: CGSize) -> Path {
+        Path { path in
+            let fill = size.height * (1 - (waterLevel < maxFillLevel ? waterLevel : maxFillLevel))
+            let width = size.width
+            let maxOffset = size.height / 4
+            path.move(to: CGPoint(x: 0, y: fill))
+            path.addCurve(to: CGPoint(x: width, y: fill),
+                          control1: CGPoint(x: width * 0.5,
+                                            y: fill + maxOffset / 100 * offsetPercent),
+                          control2: CGPoint(x: width * 0.5,
+                                            y: fill - maxOffset / 100 * offsetPercent))
+
+            path.addLine(to: CGPoint(x: width, y: size.height))
+            path.addLine(to: CGPoint(x: 0, y: size.height))
         }
     }
 }
