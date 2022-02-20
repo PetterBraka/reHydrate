800296ca65393172476db95c376b7369305f5cae
PetterBraka <Petter.braka@gmail.com>
2022-02-20 10:13:16 +0000

    updated var and added doc and init

--- reHydrate/View/AppView.swift
+++ reHydrate/View/AppView.swift
@@ -12,20 +12,28 @@ import SwiftUI
 struct AppView: View {
     @StateObject var viewModel = MainAssembler.shared.container.resolve(AppViewModel.self)!
     @State var homeTransition: AnyTransition = .slide
+    @State var waterLevel: CGFloat = 0.3
 
     var body: some View {
         ZStack {
-            Color.background
-                .ignoresSafeArea()
-            TabView(selection: $viewModel.currenState) {
-                SettingsView(navigateTo: viewModel.navigateTo)
-                    .tag(AppState.settings)
-                HomeView(navigateTo: viewModel.navigateTo)
-                    .tag(AppState.home)
-                CalendarView(navigateTo: viewModel.navigateTo)
-                    .tag(AppState.calendar)
+//            Color.background
+//                .ignoresSafeArea()
+//            TabView(selection: $viewModel.currenState) {
+//                SettingsView(navigateTo: viewModel.navigateTo)
+//                    .tag(AppState.settings)
+//                HomeView(navigateTo: viewModel.navigateTo)
+//                    .tag(AppState.home)
+//                CalendarView(navigateTo: viewModel.navigateTo)
+//                    .tag(AppState.calendar)
+//            }
+//            .tabViewStyle(.page(indexDisplayMode: .never))
+            WaveView(color: .blue, offsetPercent: 10, fillLevel: $waterLevel) {
+                Image.largeBottle
+                    .resizable()
+                    .opacity(0.5)
             }
-            .tabViewStyle(.page(indexDisplayMode: .never))
+            .aspectRatio(contentMode: .fit)
+            .frame(height: 500)
         }
         .onAppear {
             Analytics.track(event: .startUp)
