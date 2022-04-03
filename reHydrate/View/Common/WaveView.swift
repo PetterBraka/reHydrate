ac1cc73b7671befb6601a968760f05218c218eed
PetterBraka <Petter.braka@gmail.com>
2022-04-03 20:23:28 +0100

    Added  check for what day you reached goal

--- reHydrate/View/Common/WaveView.swift
+++ reHydrate/View/Common/WaveView.swift
@@ -88,6 +88,7 @@ struct WaveView<Content: View>: View {
             container
             GeometryReader { proxy in
                 Text(currentFill)
+                    .font(.body)
                     .padding(8)
                     .foregroundColor(.labelHighlighted)
                     .background(
