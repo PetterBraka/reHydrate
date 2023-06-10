9d2b9f3cb2b7987a59a979191f105e1074c2b677
Petter Brakalsvalet <Petter.Braka@gmail.com>
2023-06-10 19:40:36 +0100

    added place holder for actions

--- EngineKit/Sources/Presentation/Screens/Home/Screen.Home.Presenter.swift
+++ EngineKit/Sources/Presentation/Screens/Home/Screen.Home.Presenter.swift
@@ -8,8 +8,30 @@
 import HomePresentationInterface
 
 public extension Screen.Home {
+    typealias Engine = (
+        AnyObject
+    )
+    typealias Router = (
+        AnyObject
+    )
+    typealias Tracker = (
+        AnyObject
+    )
+    
     final class HomePresenter: HomePresenterType {
         public func perform(action: Home.Action) {
+            switch action {
+            case .didTapHistory:
+                <#code#>
+            case .didTapSettings:
+                <#code#>
+            case let .didTapAddDrink(drink):
+                <#code#>
+            case let .didTapEditDrink(drink):
+                <#code#>
+            case let .didTapRemoveDrink(drink):
+                <#code#>
+            }
         }
     }
 }
