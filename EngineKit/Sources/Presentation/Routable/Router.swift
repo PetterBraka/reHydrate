fc545ffa2f7af81bd55d71b63c8da54069215a63
Petter Brakalsvalet <Petter.Braka@gmail.com>
2023-06-11 18:16:29 +0100

    created start of router

--- EngineKit/Sources/Presentation/Routable/Router.swift
+++ EngineKit/Sources/Presentation/Routable/Router.swift
@@ -1,8 +1,29 @@
 //
-//  File.swift
+//  Router.swift
 //  
 //
 //  Created by Petter vang Brakalsvålet on 11/06/2023.
 //
 
-import Foundation
+import HomePresentationInterface
+
+public final class Router {
+    public init() {}
+}
+
+extension Router: HomeRoutable {
+    public func showSettings() {
+        // TODO: Show Settings
+        print("Show settings")
+    }
+    
+    public func showHistory() {
+        // TODO: Show History
+        print("Show history")
+    }
+    
+    public func showEdit(drink: Home.ViewModel.Drink) {
+        // TODO: Show Edit
+        print("Show edit - \(String(describing: drink))")
+    }
+}
