d9189ef56619953332f83f65ad4b6a482315a57e
Petter Brakalsvalet <Petter.Braka@gmail.com>
2023-06-12 08:14:03 +0100

    adopted drink service in sceneFactory

--- reHydrate/Utilities/SceneFactory.swift
+++ reHydrate/Utilities/SceneFactory.swift
@@ -8,6 +8,8 @@
 
 import EngineKit
 import Presentation
+import HomePresentationInterface
+import DrinkServiceInterface
 
 public final class SceneFactory {
     public static let shared = SceneFactory()
@@ -20,15 +22,41 @@ public final class SceneFactory {
     func makeHomeScreen() -> HomeScreen {
         let presenter = Screen.Home.Presenter(engine: engine,
                                               router: router)
+        var drinks: [DrinkServiceInterface.Drink]
+        let result = engine.drinksService.getSavedDrinks()
+        if case .success(let foundDrinks) = result, !foundDrinks.isEmpty {
+            drinks = foundDrinks
+        } else {
+            drinks = engine.drinksService.resetToDefault()
+        }
         
         let observer = HomeScreenObservable(presenter: presenter,
                                             date: .now, consumed: 0, goal: 3,
-                                            drinks: [.init(size: 300, container: .small),
-                                                     .init(size: 500, container: .medium),
-                                                     .init(size: 750, container: .large)],
+                                            drinks: drinks.map { .init(from: $0) },
                                             unit: (small: .milliliters, large: .liters))
         presenter.scene = observer
         
         return HomeScreen(observer: observer)
+    }
+}
+
+extension Home.ViewModel.Drink {
+    init(from drink: DrinkServiceInterface.Drink) {
+        self = .init(id: drink.id,
+                     size: drink.size,
+                     container: .init(from: drink.container))
+    }
+}
+
+extension Home.ViewModel.Container {
+    init(from container: DrinkServiceInterface.Container) {
+        switch container {
+        case .large:
+            self = .large
+        case .medium:
+            self = .medium
+        case .small:
+            self = .small
+        }
     }
 }
