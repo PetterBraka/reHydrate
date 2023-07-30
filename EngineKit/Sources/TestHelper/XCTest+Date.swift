ef0df4f9eee3d83ec64b72d6cfd12ca3dae60e67
Petter Brakalsvalet <Petter.Braka@gmail.com>
2023-07-30 10:09:13 +0100

    Created TestHelper

---
+++ EngineKit/Sources/TestHelper/XCTest+Date.swift
@@ -0,0 +1,18 @@
+//
+//  File.swift
+//  
+//
+//  Created by Petter vang Brakalsvålet on 29/07/2023.
+//
+
+import XCTest
+
+public extension XCTest {
+    static let referenceDate = Date(timeIntervalSince1970: 1688227143)
+    static let referenceDates = [
+        Date(timeIntervalSince1970: 1688227143),
+        Date(timeIntervalSince1970: 1688324062),
+        Date(timeIntervalSince1970: 1688410462),
+        Date(timeIntervalSince1970: 1688583262)
+    ]
+}
