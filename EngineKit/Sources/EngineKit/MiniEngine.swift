5e2d90285ca25ce939feb679b5b4cebc9c4fbc2f
Petter Brakalsvalet <Petter.Braka@gmail.com>
2024-02-10 14:21:40 +0000

    Added mini engine for watch and widgets

---
+++ EngineKit/Sources/EngineKit/MiniEngine.swift
@@ -0,0 +1,46 @@
+//
+//  File.swift
+//
+//
+//  Created by Petter vang Brakalsvålet on 10/02/2024.
+//
+
+import Foundation
+import LoggingService
+import UnitServiceInterface
+import UnitService
+import UserPreferenceServiceInterface
+import UserPreferenceService
+import DateServiceInterface
+import DateService
+
+// The Mini engine is intended for app extensions (Watch & Widgets)
+public final class MiniEngine {
+    private let sharedDefaults: UserDefaults
+    private let subsystem: String
+    
+    public init(
+        appGroup: String,
+        subsystem: String
+    ) {
+        self.subsystem = subsystem
+        
+        guard let sharedDefaults = UserDefaults(suiteName: appGroup)
+        else {
+            fatalError("Shared UserDefaults couldn't be setup")
+        }
+        self.sharedDefaults = sharedDefaults
+    }
+    
+    public lazy var logger: LoggingService = LoggingService(subsystem: subsystem)
+    public lazy var userPreferenceService: UserPreferenceServiceType = UserPreferenceService(defaults: sharedDefaults)
+    public lazy var unitService: UnitServiceType = UnitService(engine: self)
+    public lazy var dateService: DateServiceType = DateService(calendar: .current)
+}
+
+extension MiniEngine: 
+    HasLoggingService,
+    HasUnitService,
+    HasUserPreferenceService,
+    HasDateService
+{}
