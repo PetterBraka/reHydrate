4df3125b4e3c1636f449e8016cd9f94964ad3bd0
Petter Brakalsvalet <Petter.Braka@gmail.com>
2024-02-17 15:32:16 +0000

    added tests for `isDateInSameDayAs`

--- EngineKit/Sources/DateService/Tests/DateServiceTests.swift
+++ EngineKit/Sources/DateService/Tests/DateServiceTests.swift
@@ -19,36 +19,36 @@ final class DateServiceTests: XCTestCase {
 }
 // MARK: - getEnd tests
 extension DateServiceTests {
-    func test_getEnd_june_10_2018() throws {
+    func test_getEnd_june_10_2018() {
         let date = sut.getEnd(of: .june_10_2018_Sunday)
         
-        try assert(givenDate: date,
-                   expectedYear: 2018, expectedMonth: 6, expectedDay: 10,
-                   expectedHours: 23, expectedMinutes: 59, expectedSeconds: 59)
+        assert(givenDate: date,
+               expectedYear: 2018, expectedMonth: 6, expectedDay: 10,
+               expectedHours: 23, expectedMinutes: 59, expectedSeconds: 59)
     }
     
-    func test_getEnd_february_1_2024() throws {
+    func test_getEnd_february_1_2024() {
         let date = sut.getEnd(of: .february_1_2024_Thursday)
         
-        try assert(givenDate: date,
+        assert(givenDate: date,
                expectedYear: 2024, expectedMonth: 2, expectedDay: 1,
                expectedHours: 23, expectedMinutes: 59, expectedSeconds: 59)
     }
     
-    func test_getEnd_november_3_1966() throws {
+    func test_getEnd_november_3_1966() {
         let date = sut.getEnd(of: .november_3_1966_Thursday)
         
-        try assert(givenDate: date,
+        assert(givenDate: date,
                expectedYear: 1966, expectedMonth: 11, expectedDay: 3,
                expectedHours: 23, expectedMinutes: 59, expectedSeconds: 59)
     }
     
-    func test_getEnd_march_7_1994() throws {
+    func test_getEnd_march_7_1994() {
         let date = sut.getEnd(of: .march_5_1970_Thursday)
         
-        try assert(givenDate: date,
-                   expectedYear: 1970, expectedMonth: 3, expectedDay: 5,
-                   expectedHours: 23, expectedMinutes: 59, expectedSeconds: 59)
+        assert(givenDate: date,
+               expectedYear: 1970, expectedMonth: 3, expectedDay: 5,
+               expectedHours: 23, expectedMinutes: 59, expectedSeconds: 59)
     }
 }
 
@@ -65,7 +65,7 @@ extension DateServiceTests {
     }
     
     func test_daysBetween_february_2_1998() {
-        let days = sut.daysBetween(.init(year: 1998, month: 2, day: 2)!,
+        let days = sut.daysBetween(.init(year: 1998, month: 2, day: 2),
                                    end: .february_1_2024_Thursday)
         XCTAssertEqual(days, 9_495)
     }
@@ -83,29 +83,62 @@ extension DateServiceTests {
 
 // MARK: - getDate tests
 extension DateServiceTests {
-    func test_getDate_possitive10() throws {
+    func test_getDate_positive10() {
         let date = sut.getDate(byAddingDays: 10, to: .january_1_2024_Monday)
-        try assert(givenDate: date, expectedYear: 2024, expectedMonth: 1, expectedDay: 11)
+        assert(givenDate: date, expectedYear: 2024, expectedMonth: 1, expectedDay: 11)
     }
     
-    func test_getDate_possitive366() throws {
+    func test_getDate_positive366() {
         let date = sut.getDate(byAddingDays: 366, to: .january_1_2024_Monday)
-        try assert(givenDate: date, expectedYear: 2025, expectedMonth: 1, expectedDay: 1)
+        assert(givenDate: date, expectedYear: 2025, expectedMonth: 1, expectedDay: 1)
     }
     
-    func test_getDate_possitive730() throws {
+    func test_getDate_positive730() {
         let date = sut.getDate(byAddingDays: 730, to: .january_1_2024_Monday)
-        try assert(givenDate: date, expectedYear: 2025, expectedMonth: 12, expectedDay: 31)
+        assert(givenDate: date, expectedYear: 2025, expectedMonth: 12, expectedDay: 31)
     }
     
-    func test_getDate_negative8() throws {
+    func test_getDate_negative8() {
         let date = sut.getDate(byAddingDays: -8, to: .february_6_1994_Sunday)
-        try assert(givenDate: date, expectedYear: 1994, expectedMonth: 1, expectedDay: 29)
+        assert(givenDate: date, expectedYear: 1994, expectedMonth: 1, expectedDay: 29)
     }
     
-    func test_getDate_negative60() throws {
+    func test_getDate_negative60() {
         let date = sut.getDate(byAddingDays: -60, to: .february_6_1994_Sunday)
-        try assert(givenDate: date, expectedYear: 1993, expectedMonth: 12, expectedDay: 8)
+        assert(givenDate: date, expectedYear: 1993, expectedMonth: 12, expectedDay: 8)
+    }
+}
+
+// MARK: - isDateInSameDayAs tests
+extension DateServiceTests {
+    func test_isDate_isSameDate() {
+        let isSameDay = sut.isDate(.february_1_2024_Thursday,
+                                   inSameDayAs: .february_1_2024_Thursday)
+        XCTAssertEqual(isSameDay, true)
+    }
+    
+    func test_isDate_isNotSameDateButDay() {
+        let isSameDay = sut.isDate(.february_6_1994_Sunday,
+                                   inSameDayAs: .init(year: 2024, month: 1, day: 6))
+        XCTAssertEqual(isSameDay, false)
+    }
+    
+    func test_isDate_isNotSameDateButMonthAndDay() {
+        let isSameDay = sut.isDate(.february_6_1994_Sunday,
+                                   inSameDayAs: .init(year: 2024, month: 2, day: 6))
+        XCTAssertEqual(isSameDay, false)
+    }
+    
+    func test_isDate_isNotSameDateButMonth() {
+        let isSameDay = sut.isDate(.february_6_1994_Sunday,
+                                   inSameDayAs: .init(year: 2024, month: 2, day: 1))
+        XCTAssertEqual(isSameDay, false)
+    }
+    
+    func test_isDate_isNotSameDateButYearAndMonth() {
+        let isSameDay = sut.isDate(.february_6_1994_Sunday,
+                                   inSameDayAs: .init(year: 1994, month: 2, day: 1))
+        XCTAssertEqual(isSameDay, false)
     }
 }
 
@@ -113,16 +146,15 @@ extension DateServiceTests {
     func assert(
         givenDate: Date?,
         expectedYear year: Int, expectedMonth month: Int, expectedDay day: Int,
-        expectedHours hours: Int = 0, 
+        expectedHours hours: Int = 0,
         expectedMinutes minutes: Int = 0,
         expectedSeconds seconds: Int = 0,
         file: StaticString = #file, line: UInt = #line
-    ) throws {
-        try XCTAssertEqual(
+    ) {
+        XCTAssertEqual(
             givenDate,
-            XCTUnwrap(Date(year: year, month: month, day: day,
-                           hours: hours, minutes: minutes, seconds: seconds),
-                           file: file, line: line),
+            Date(year: year, month: month, day: day,
+                 hours: hours, minutes: minutes, seconds: seconds),
             file: file, line: line
         )
     }
