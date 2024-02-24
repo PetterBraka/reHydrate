dbef19f27a691c62bdee37b4fb98a577f812a9a6
Petter Brakalsvalet <Petter.Braka@gmail.com>
2024-02-24 17:05:26 +0000

    started setting up tests

--- PresentationKit/Sources/PresentationTests/HomePresentationTests.swift
+++ PresentationKit/Sources/PresentationTests/HomePresentationTests.swift
@@ -7,16 +7,23 @@
 
 import XCTest
 import EngineMocks
+@testable import PresentationKit
 
 final class HomePresentationTests: XCTestCase {
+    private var sut: Screen.Home.Presenter!
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    override func setUp() {
+        let engine = EngineMocks()
+        sut = Screen.Home.Presenter(
+            engine: engine,
+            router: Router()
+        )
     }
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
+    
 
     func testExample() throws {
         // This is an example of a functional test case.
