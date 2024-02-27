6acc6b1959e61a797c5aa90462edd9e8d4ab5f0b
Petter Brakalsvalet <Petter.Braka@gmail.com>
2024-02-27 20:37:26 +0000

    missed regen

--- EngineKit/Sources/EngineMocks/Generated/EngineMocks.generated.swift
+++ EngineKit/Sources/EngineMocks/Generated/EngineMocks.generated.swift
@@ -38,84 +38,84 @@ extension EngineMocks {
         let stub = NotificationServiceTypeStub()
         let spy = NotificationServiceTypeSpy(realObject: stub)
 
-        self.notificationService = stub
+        self.notificationService = spy
         return (stub, spy)
     }
     public func makeNotificationDelegate() -> (stub: NotificationDelegateTypeStubbing, spy: NotificationDelegateTypeSpying) {
         let stub = NotificationDelegateTypeStub()
         let spy = NotificationDelegateTypeSpy(realObject: stub)
 
-        self.notificationDelegate = stub
+        self.notificationDelegate = spy
         return (stub, spy)
     }
     public func makeAppearancePort() -> (stub: AppearancePortTypeStubbing, spy: AppearancePortTypeSpying) {
         let stub = AppearancePortTypeStub()
         let spy = AppearancePortTypeSpy(realObject: stub)
 
-        self.appearancePort = stub
+        self.appearancePort = spy
         return (stub, spy)
     }
     public func makeAlternateIconsService() -> (stub: AlternateIconsServiceTypeStubbing, spy: AlternateIconsServiceTypeSpying) {
         let stub = AlternateIconsServiceTypeStub()
         let spy = AlternateIconsServiceTypeSpy(realObject: stub)
 
-        self.alternateIconsService = stub
+        self.alternateIconsService = spy
         return (stub, spy)
     }
     public func makeOpenUrlService() -> (stub: OpenUrlInterfaceStubbing, spy: OpenUrlInterfaceSpying) {
         let stub = OpenUrlInterfaceStub()
         let spy = OpenUrlInterfaceSpy(realObject: stub)
 
-        self.openUrlService = stub
+        self.openUrlService = spy
         return (stub, spy)
     }
     public func makeHealthService() -> (stub: HealthInterfaceStubbing, spy: HealthInterfaceSpying) {
         let stub = HealthInterfaceStub()
         let spy = HealthInterfaceSpy(realObject: stub)
 
-        self.healthService = stub
+        self.healthService = spy
         return (stub, spy)
     }
     public func makeDayService() -> (stub: DayServiceTypeStubbing, spy: DayServiceTypeSpying) {
         let stub = DayServiceTypeStub()
         let spy = DayServiceTypeSpy(realObject: stub)
 
-        self.dayService = stub
+        self.dayService = spy
         return (stub, spy)
     }
     public func makeDrinksService() -> (stub: DrinkServiceTypeStubbing, spy: DrinkServiceTypeSpying) {
         let stub = DrinkServiceTypeStub()
         let spy = DrinkServiceTypeSpy(realObject: stub)
 
-        self.drinksService = stub
+        self.drinksService = spy
         return (stub, spy)
     }
     public func makeLanguageService() -> (stub: LanguageServiceTypeStubbing, spy: LanguageServiceTypeSpying) {
         let stub = LanguageServiceTypeStub()
         let spy = LanguageServiceTypeSpy(realObject: stub)
 
-        self.languageService = stub
+        self.languageService = spy
         return (stub, spy)
     }
     public func makeUnitService() -> (stub: UnitServiceTypeStubbing, spy: UnitServiceTypeSpying) {
         let stub = UnitServiceTypeStub()
         let spy = UnitServiceTypeSpy(realObject: stub)
 
-        self.unitService = stub
+        self.unitService = spy
         return (stub, spy)
     }
     public func makeAppearanceService() -> (stub: AppearanceServiceTypeStubbing, spy: AppearanceServiceTypeSpying) {
         let stub = AppearanceServiceTypeStub()
         let spy = AppearanceServiceTypeSpy(realObject: stub)
 
-        self.appearanceService = stub
+        self.appearanceService = spy
         return (stub, spy)
     }
     public func makeDateService() -> (stub: DateServiceTypeStubbing, spy: DateServiceTypeSpying) {
         let stub = DateServiceTypeStub()
         let spy = DateServiceTypeSpy(realObject: stub)
 
-        self.dateService = stub
+        self.dateService = spy
         return (stub, spy)
     }
 }
