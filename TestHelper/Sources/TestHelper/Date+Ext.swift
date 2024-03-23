27252a7a971f45bcf9d1d64030b6752dc33f5570
Petter Brakalsvalet <Petter.Braka@gmail.com>
2024-03-23 22:35:39 +0000

    tried fix for ci tests

--- TestHelper/Sources/TestHelper/Date+Ext.swift
+++ TestHelper/Sources/TestHelper/Date+Ext.swift
@@ -31,9 +31,10 @@ public extension Date {
         year: Int, month: Int, day: Int,
         hours: Int = 0, minutes: Int = 0, seconds: Int = 0
     ) {
-        let dateString = "\(day)-\(month)-\(year) \(hours):\(minutes):\(seconds) +0000"
+        let dateString = "\(day)-\(month)-\(year) \(hours):\(minutes):\(seconds)"
         let formatter = DateFormatter()
-        formatter.dateFormat = "dd-MM-yyyy HH:mm:ss Z"
+        formatter.locale = .init(identifier: "en_UK")
+        formatter.dateFormat = "dd-MM-yyyy HH:mm:ss"
         guard let date = formatter.date(from: dateString )
         else { fatalError("Invalid date - \(dateString)") }
         self = date
