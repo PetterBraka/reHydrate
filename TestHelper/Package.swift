c3e645e97bbc897cf0e297a99bd9650689c93e0a
Petter Brakalsvalet <Petter.Braka@gmail.com>
2024-03-26 18:52:28 +0000

    syntax update

--- TestHelper/Package.swift
+++ TestHelper/Package.swift
@@ -5,15 +5,6 @@ import PackageDescription
 
 let package = Package(
     name: "TestHelper",
-    products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "TestHelper",
-            targets: ["TestHelper"]),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(name: "TestHelper")
-    ]
+    products: [.library(name: "TestHelper", targets: ["TestHelper"])],
+    targets: [.target(name: "TestHelper")]
 )
