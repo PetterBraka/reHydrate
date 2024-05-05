919c8c66151797a57e64c7914efa8b89bfb126f8
Petter Brakalsvalet <Petter.Braka@gmail.com>
2024-05-05 13:10:32 +0100

    fixed build error

--- PresentationWatchKit/Package.swift
+++ PresentationWatchKit/Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     name: "PresentationWatchKit",
     platforms: [
         .watchOS(.v10), 
-        .macOS(.v13)
+        .macOS(.v14)
     ],
     products: [
         .library(
