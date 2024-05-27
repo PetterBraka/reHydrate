64f4cc588ba555c554da5ca640543ca74d4065b4
Petter Brakalsvalet <Petter.Braka@gmail.com>
2024-05-27 07:59:28 +0100

    removed unneeded didFinish and Companion notifcation

--- CommunicationKit/Sources/WatchCommunicationKit/Interface/WatchServiceType.swift
+++ CommunicationKit/Sources/WatchCommunicationKit/Interface/WatchServiceType.swift
@@ -12,8 +12,6 @@ public protocol WatchServiceType {
     var isReachable: Bool { get set }
     var applicationContext: [String : Any] { get set }
     var receivedApplicationContext: [String : Any] { get set }
-    
-    var isCompanionAppInstalled: Bool { get set }
     var iOSDeviceNeedsUnlockAfterRebootForReachability: Bool { get set }
     
     func isSupported() -> Bool
@@ -21,5 +19,5 @@ public protocol WatchServiceType {
     func update(applicationContext: [String : Any]) throws
     func send(message: [String : Any], replyHandler: (([String : Any]) -> Void)?, errorHandler: ((Error) -> Void)?)
     func send(messageData data: Data, replyHandler: ((Data) -> Void)?, errorHandler: ((Error) -> Void)?)
-    func transfer(userInfo: [String : Any]) -> CommunicationInfo
+    func send(userInfo: [String : Any]) -> CommunicationInfo
 }
