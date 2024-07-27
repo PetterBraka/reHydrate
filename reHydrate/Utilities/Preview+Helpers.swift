0e6146fc2da6d3a90ba8dd957d22c01bd4cc58d6
Petter Brakalsvalet <Petter.Braka@gmail.com>
2024-07-27 22:22:35 +0100

    updated mock helper to use EngineMocks

--- reHydrate/Utilities/Preview+Helpers.swift
+++ reHydrate/Utilities/Preview+Helpers.swift
@@ -6,37 +6,9 @@
 //  Copyright © 2023 Petter vang Brakalsvålet. All rights reserved.
 //
 
-import UserNotifications
 import EngineKit
-import LoggingService
-import PortsInterface
-import DBKit
-import DBKitInterface
-import DBKitMocks
+import EngineMocks
 
 extension Engine {
-    typealias EngineType = (
-        HasService &
-        HasAppInfo &
-        HasDayManagerService &
-        HasDrinkManagerService &
-        HasConsumptionManagerService &
-        HasPorts
-    )
-    
-    static let mock: EngineType = Engine(
-        appGroup: "com.mock",
-        appVersion: "1.0.0",
-        logger: LoggingService(subsystem: "com.mock"),
-        dayManager: DayManager(database: DatabaseStub()),
-        drinkManager: DrinkManager(database: DatabaseStub()),
-        consumptionManager: ConsumptionManager(database: DatabaseStub()),
-        reminders: [.init(title: "Test", body: "Test")],
-        celebrations: [.init(title: "Test", body: "Test")],
-        notificationCenter: UNUserNotificationCenter.current(), 
-        openUrlService: OpenUrlPort(),
-        alternateIconsService: AlternateIconsServicePort(),
-        appearancePort: AppearanceServicePort(),
-        healthService: HealthKitPort()
-    )
+    static let mock = EngineMocks()
 }
